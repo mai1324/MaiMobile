92934f5519142975feec8a3964cd8ff4522e68a2
Mike <mai1324@uom.edu.gr>
2013-01-12 20:18:24 +0200

    Final commit!

--- MaiMobile/gen/com/example/maimobile/R.java
+++ MaiMobile/gen/com/example/maimobile/R.java
@@ -11,11 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int blue=0x7f040001;
         public static final int yellow=0x7f040000;
     }
     public static final class dimen {
         public static final int dekaeksi=0x7f050007;
         public static final int dekari=0x7f050008;
+        public static final int dekawxtari=0x7f050009;
         public static final int dwdekari=0x7f050003;
         public static final int eikosi=0x7f050005;
         public static final int eikosidio=0x7f050004;
@@ -26,31 +28,48 @@ public final class R {
     }
     public static final class drawable {
         public static final int contact_button=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int info_button=0x7f020002;
-        public static final int machina_logo=0x7f020003;
-        public static final int new_button=0x7f020004;
-        public static final int program_button=0x7f020005;
+        public static final int exams_button=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int info_button=0x7f020003;
+        public static final int kanonismos_button=0x7f020004;
+        public static final int machina_logo=0x7f020005;
+        public static final int map_button=0x7f020006;
+        public static final int new_button=0x7f020007;
+        public static final int program_button=0x7f020008;
+        public static final int pushpin=0x7f020009;
+        public static final int study_button=0x7f02000a;
+        public static final int vasiko_background1=0x7f02000b;
+        public static final int weekly_button=0x7f02000c;
     }
     public static final class id {
-        public static final int button1=0x7f090001;
-        public static final int button2=0x7f090000;
-        public static final int button3=0x7f090003;
-        public static final int button4=0x7f090002;
-        public static final int menu_settings=0x7f090008;
-        public static final int textView1=0x7f090004;
-        public static final int textView2=0x7f090005;
+        public static final int button1=0x7f090002;
+        public static final int button2=0x7f090003;
+        public static final int button3=0x7f090005;
+        public static final int button4=0x7f090004;
+        public static final int mapView=0x7f090008;
+        public static final int menu_settings=0x7f09000b;
+        public static final int myWebView=0x7f09000a;
+        public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090001;
         public static final int textView3=0x7f090006;
         public static final int textView4=0x7f090007;
+        public static final int zoom=0x7f090009;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int contact=0x7f030000;
+        public static final int info=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int maps=0x7f030003;
+        public static final int news=0x7f030004;
+        public static final int program=0x7f030005;
+        public static final int webpage=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int app_name=0x7f060000;
+        public static final int contact=0x7f060003;
         public static final int hello_world=0x7f060001;
         public static final int menu_settings=0x7f060002;
     }
--- MaiMobile/src/com/example/maimobile/Main.java
+++ MaiMobile/src/com/example/maimobile/Main.java
@@ -2,7 +2,11 @@ package com.example.maimobile;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
 
@@ -10,6 +14,43 @@ public class Main extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		Button news = (Button) findViewById(R.id.button1);
+        news.setOnClickListener(new View.OnClickListener() {
+            
+        	public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), New.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        
+		Button contact = (Button) findViewById(R.id.button4);
+        contact.setOnClickListener(new View.OnClickListener() {
+            
+        	public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Contact.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        
+		Button info = (Button) findViewById(R.id.button2);
+        info.setOnClickListener(new View.OnClickListener() {
+            
+        	public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Info.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+        
+		Button program = (Button) findViewById(R.id.button3);
+        program.setOnClickListener(new View.OnClickListener() {
+            
+        	public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Program.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
 	}
 
 	@Override
